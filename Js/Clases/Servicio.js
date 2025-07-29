2a6055d71c9f83f9d1153fbdb5a520efd608bc31
BrThompson2312 <brunogomez2312@gmail.com>
2025-07-28 23:16:24 -0300

    fixing part 1

--- Js/Clases/Servicio.js
+++ Js/Clases/Servicio.js
@@ -14,8 +14,8 @@ class Servicio {
 }
 
 const servicios = [
-    new Servicio("Corte de Pelo", "Corte clásico o moderno, adaptado al estilo y preferencias del cliente. Incluye lavado exprés y acabado con productos de styling.", 30, 600, null),    
-    new Servicio("Perfilado y Afeitado de Barba", "Perfilado de barba con máquina y/o navaja, incluye vapor caliente, loción refrescante y toalla tibia.", 30, 450, null),  
-    new Servicio("Coloración Capilar", "Aplicación de color para cubrir canas o dar un estilo personalizado. Incluye diagnóstico previo y productos sin amoníaco.", 30, 1000, null),  
-    new Servicio("Manicura Masculina", "Corte y limado de uñas, tratamiento de cutículas, hidratación y pulido (sin esmalte). Ideal para imagen profesional o cuidado personal.", 30, 500, null), 
+    new Servicio("Corte de Pelo", "Corte clásico o moderno, adaptado al estilo y preferencias del cliente. Incluye lavado exprés y acabado con productos de styling.", 30, 600, "Imgs/cortedepelo.jpg"),    
+    new Servicio("Perfilado y Afeitado de Barba", "Perfilado de barba con máquina y/o navaja, incluye vapor caliente, loción refrescante y toalla tibia.", 30, 450, "Imgs/cortedepelo.jpg"),  
+    new Servicio("Coloración Capilar", "Aplicación de color para cubrir canas o dar un estilo personalizado. Incluye diagnóstico previo y productos sin amoníaco.", 30, 1000, "Imgs/cortedepelo.jpg"),  
+    new Servicio("Manicura Masculina", "Corte y limado de uñas, tratamiento de cutículas, hidratación y pulido (sin esmalte). Ideal para imagen profesional o cuidado personal.", 30, 500, "Imgs/cortedepelo.jpg"), 
 ];
--- Js/Main.js
+++ Js/Main.js
@@ -3,10 +3,10 @@ let $ = (e) => document.querySelector(e);
 function importar_promociones() {
     promociones.forEach(e => {
         $("#promociones").innerHTML += `
-            <div class="box">
+            <div class="box d-flex flex-column justify-content-between">
                 <h3>${e.nombre}</h3>
                 <p>${e.descripcion}</p>
-                <p><strong>Precio: ${e.precio}<strong></p>
+                <p><strong>Precio: $${e.precio}</strong></p>
             </div>
         `;
     })
@@ -16,13 +16,15 @@ function importar_servicios() {
     servicios.forEach(e => {
         $("#servicios").innerHTML += `
             <div class="box">
-                <div>
-                    <h3>${e.nombre}</h3>
-                    <p>${e.descripcion}</p>
-                    <p><strong>Duracion estimada: ${e.duracion}<strong></p>
-                    <p><strong>Precio: ${e.precio}<strong></p>
+                <h3>${e.nombre}</h3>
+                <div class="d-flex align-items-center justify-content-between gap-1">
+                    <div class="info-corte d-flex align-items-center justify-content-between flex-column gap-1">
+                        <p>${e.descripcion}</p>
+                        <p><strong>Duracion estimada: ${e.duracion}</strong></p>
+                        <p><strong>Precio: ${e.precio}</strong></p>
+                    </div>
+                    <img src="${e.imagen}">
                 </div>
-                <img src="${e.imagen}">
             </div>
         `;
     })
@@ -31,7 +33,7 @@ function importar_servicios() {
 function importar_barberos() {
     barberos.forEach(e => {
         $("#barberos").innerHTML += `
-            <div class="box">
+            <div class="box d-flex align-items-start justify-content-start flex-column">
                 <img class="bg-secondary d-block mb-3 mx-auto" src="${e.foto}">
                 <div class="text-start pb-5">
                     <h4>${e.nombre}</h4>
@@ -51,5 +53,3 @@ function importar_data() {
 importar_data();
 
 localStorage.setItem("datos", JSON.stringify(reservas));
-
-console.log(localStorage.getItem("datos"));
